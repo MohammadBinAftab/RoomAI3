1fe507a8ba752005327a654ae38d0b8faaeb6a5e
MohammadBinAftab <48010758+MohammadBinAftab@users.noreply.github.com>
2024-11-12 23:32:03 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,32 +1,11 @@
-// pages/_app.js
-"use client";
-
-import { SessionProvider as NextAuthSessionProvider } from "next-auth/react";
-import { useSession } from "next-auth/react";
-import { createContext, useContext } from "react";
-
-const GlobalSessionContext = createContext();
-
-function GlobalSessionProvider({ children }) {
-  const sessionData = useSession(); // Call useSession globally here
-  return (
-    <GlobalSessionContext.Provider value={sessionData}>
-      {children}
-    </GlobalSessionContext.Provider>
-  );
-}
-
-export function useGlobalSession() {
-  return useContext(GlobalSessionContext); // Access useSession anywhere
-}
+import { SessionProvider } from 'next-auth/react';
+import '../styles/globals.css'; 
 
 function MyApp({ Component, pageProps }) {
   return (
-    <NextAuthSessionProvider session={pageProps.session}>
-      <GlobalSessionProvider>
-        <Component {...pageProps} />
-      </GlobalSessionProvider>
-    </NextAuthSessionProvider>
+    <SessionProvider session={pageProps.session}>
+      <Component {...pageProps} />
+    </SessionProvider>
   );
 }
 
